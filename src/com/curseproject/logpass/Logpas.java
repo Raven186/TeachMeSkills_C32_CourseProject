559ad4b047bf45de4b7c0faa714e1cb40e70aaec
HmaraDmitriy <firmasa191@gmail.com>
2024-12-08 18:10:41 +0300

    Logs and pass

---
+++ src/com/curseproject/logpass/Logpas.java
@@ -0,0 +1,13 @@
+package com.curseproject.logpass;
+
+
+import com.curseproject.logpass.ussers.User;
+
+public class Logpas {
+    public static void main(String[] args) {
+
+        User user1 = new User("Admin","2tg2f5ywy5f5");
+
+        System.out.println(user1);
+    }
+}
